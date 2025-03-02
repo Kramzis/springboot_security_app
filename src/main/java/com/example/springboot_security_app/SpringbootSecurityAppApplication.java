1137fc07c7fb623cf497668a34cb17c7c213a137
Karol Mateusz Kurowski <s95465@pollub.edu.pl>
2025-03-02 20:19:24 +0100

    Initial commit

---
+++ src/main/java/com/example/springboot_security_app/SpringbootSecurityAppApplication.java
@@ -0,0 +1,13 @@
+package com.example.springboot_security_app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootSecurityAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootSecurityAppApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/springboot_security_app/SpringbootSecurityAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springboot_security_app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootSecurityAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
