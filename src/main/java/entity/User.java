311904a48ba45b8c4c8880954c21fac09e345477
Kramzis <s95453@pollub.edu.pl>
2025-03-02 23:19:28 +0100

    1, continuation of adding user feature

--- src/main/java/entity/User.java
+++ src/main/java/entity/User.java
@@ -1,4 +1,23 @@
 package entity;
 
+import javax.persistence.*;
+import java.util.List;
+
+@Entity
+@Table(name = "user")
+
 public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+
+    @Column(unique = true, nullable = false)
+    private String email;
+
+    @ElementCollection(fetch = FetchType.EAGER)
+    @CollectionTable(name = "user_roles", joinColumns = @JoinColumn(name = "user_id"))
+    @Column(name = "roles")
+    private List<String> roles;
+    private String password;
+    private boolean isVerified = false;
 }
